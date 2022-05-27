8b245279bbc789eb6cfb400fe78e23d6b0b32896
tugia <tugiadinh117@gmail.com>
2022-05-27 12:46:09 +0700

    remove unused code

--- Request.h
+++ Request.h
@@ -3,26 +3,27 @@
 #define REQUEST_H
 using namespace std;
 class Request{
-    //status -1: reject, 0: Pending, 1:Accepted
-    int status;
-    string renterName;
-    string startDay;
+    private:
+        //status -1: reject, 0: Pending, 1:Accepted
+        int status;
+        string renterName;
+        string startDay;
     public:
 
-    Request() {};
-    Request(string renterName, string startDay);
-    //getter
-    string getRenterName();
-    string getStartDay();
-    int getStatus();
+        Request() {};
+        Request(string renterName, string startDay);
+        //getter
+        string getRenterName();
+        string getStartDay();
+        int getStatus();
 
-    //setter
-    void setRenterName(string renterName);
-    void setStartDay(string startDay);
-    void setStatus(int status);
-    
-    void showRequest();
-    string to_string();
+        //setter
+        void setRenterName(string renterName);
+        void setStartDay(string startDay);
+        void setStatus(int status);
+
+        void showRequest();
+        string to_string();
 };
 
 #endif
